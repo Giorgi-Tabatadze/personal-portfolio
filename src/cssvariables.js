77f1df984e782ca1bdb44162a5ecd1c6f9e1b8eb
Giorgi-Tabatadze <giorgi.taba@icloud.com>
2022-10-04 18:48:57 +0400

    set up starting css

---
+++ src/cssvariables.js
@@ -0,0 +1,10 @@
+const cssvariables = {
+  colors: {
+    ButtonsGreen: "#F6F8FA",
+    MainWhite: "#F6F8FA",
+    MainGray: "#8092b2",
+    BackgroundBlack: "#0a0f25",
+  },
+};
+
+export default cssvariables;
