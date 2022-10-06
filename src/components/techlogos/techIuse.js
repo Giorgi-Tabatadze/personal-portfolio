b30dc8f1dda36d129fc2404d9f05bf1d9610e04c
Giorgi-Tabatadze <giorgi.taba@icloud.com>
2022-10-06 18:45:26 +0400

    add logo images

---
+++ src/components/techlogos/techIuse.js
@@ -0,0 +1,45 @@
+import htmlLogo from "./html.svg";
+import cssLogo from "./css.svg";
+import javascriptLogo from "./javascript.svg";
+import reactLogo from "./react.svg";
+import jestLogo from "./jest.svg";
+import gitLogo from "./git.svg";
+import webpackLogo from "./webpack.svg";
+import npmLogo from "./npm.svg";
+
+const html = {
+  logo: htmlLogo,
+  text: "Html",
+};
+const css = {
+  logo: cssLogo,
+  text: "Css",
+};
+const javascript = {
+  logo: javascriptLogo,
+  text: "Javascript",
+};
+
+const react = {
+  logo: reactLogo,
+  text: "React",
+};
+const jest = {
+  logo: jestLogo,
+  text: "Jest",
+};
+const git = {
+  logo: gitLogo,
+  text: "Git",
+};
+const webpack = {
+  logo: webpackLogo,
+  text: "Webpack",
+};
+const npm = {
+  logo: npmLogo,
+  text: "Npm",
+};
+const techKnowladge = [html, css, javascript, react, jest, git, webpack, npm];
+
+export default techKnowladge;
